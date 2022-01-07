51ce4b5a94613edaf54f7dcb0246ff120c811992
lrodenyoder <rodenlau000@gmail.com>
2022-01-07 12:04:41 -0600

    player name bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -141,12 +141,22 @@ var shop = function () {
             break;
     }
 };
+
+var getPlayerName = function () {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = window.prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+};
 //END GAME FUNCTIONS
 
 //BEGIN GAME INFORMATION/VARIABLES
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
